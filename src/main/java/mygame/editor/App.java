ec16c68815bd2f114e5cf62e595279858895c63c
tajcig@ya.ru <tajcig@ya.ru>
2016-06-16 22:01:14 +0300

    added editor

--- src/main/java/mygame/editor/App.java
+++ src/main/java/mygame/editor/App.java
@@ -9,7 +9,18 @@ import javafx.stage.Stage;
 /**
  * Created by Admin on 11.06.2016.
  */
-public class Main  extends Application{
+public class App extends Application{
+
+    private static App INSTANCE;
+    private Stage window;
+
+    public static App getInstance(){
+        return INSTANCE;
+    }
+
+    public Stage getWindow(){
+        return window;
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -18,9 +29,10 @@ public class Main  extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        INSTANCE = this;
         Parent load = FXMLLoader.load(this.getClass().getResource("/editor.fxml"));
         primaryStage.setTitle("Editor");
-        primaryStage.setScene(new Scene(load,1000,1000));
+        primaryStage.setScene(new Scene(load,800,600));
         primaryStage.show();
     }
 }
--- src/main/java/mygame/editor/Controller.java
+++ src/main/java/mygame/editor/Controller.java
@@ -2,34 +2,103 @@ package mygame.editor;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Line;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Created by Admin on 11.06.2016.
  */
-public class Controller implements Initializable{
+public class Controller implements Initializable {
     public Pane pnGrid;
+    public TilePane tilePane;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        double width = 1000;
-        double height = 1000;
+        double width = 700;
+        double height = 700;
 
 
-        for(int i = 0 ; i < width ; i+=50){
-            Line line = new Line(0,i,width,i);
-            Line line1 = new Line(i,0,i,width);
-            pnGrid.getChildren().addAll(line,line1);
+        for (int i = 0; i < width; i += 50)
+            for (int j = 0; j < height; j += 50) {
+                Rectangle rectangle = new Rectangle(i + 2, j + 2, 46, 46);
+                Label label = new Label(j + " " + i);
+                label.setLayoutX(i);
+                label.setLayoutY(j);
 
+                rectangle.setFill(Color.WHITE);
+                rectangle.setStroke(Color.BLACK);
+                rectangle.setStrokeWidth(2);
+
+                rectangle.setArcHeight(5);
+                rectangle.setArcWidth(5);
+
+                rectangle.setOnMouseEntered(this::onMouseEnter);
+                rectangle.setOnMouseExited(this::onMouseOver);
+                pnGrid.getChildren().add(rectangle);
+                pnGrid.getChildren().add(label);
+            }
+
+    }
+
+
+    public void onMouseEnter(MouseEvent event) {
+        Rectangle rectangle = (Rectangle) event.getSource();
+        rectangle.setFill(Color.BLACK);
+
+    }
+
+    public void onMouseOver(MouseEvent event) {
+        Rectangle rectangle = (Rectangle) event.getSource();
+        rectangle.setFill(Color.WHITE);
+    }
+
+
+    public void onAbout(ActionEvent event) {
+        Dialog.showDialog("Tile builder editor by vipjoker");
+    }
+
+    public void onAdd(ActionEvent event) {
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open Resource File");
+        Stage window = App.getInstance().getWindow();
+        File imageDir = directoryChooser.showDialog(window);
+        if (imageDir != null) {
+
+            for(File file :imageDir.listFiles())
+            addTile(file);
         }
     }
 
-    public void onClick(ActionEvent event){
+    private void addTile(File file) {
 
+        Image image = null;
+        try {
+            image = new Image(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+
+        tilePane.getChildren().add(imageView);
     }
+
+
 }
